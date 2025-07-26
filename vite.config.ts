4d03cd5f9a377357d25e6850129cb0420695b7b3
DuVanSang <146832513+DuVanSang@users.noreply.github.com>
2025-07-26 14:01:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path' // ✅ thêm dòng này
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // ✅ sửa lại dòng này
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
